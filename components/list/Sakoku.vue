cdee3b041375a5a45f0b36fc4b60dc7c2d5eb364
ando <ando@torat.jp>
2023-01-08 18:03:31 +0900

    add

--- components/list/Sakoku.vue
+++ components/list/Sakoku.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <nuxt-link to="/page/sakoku/" class="group h-100 block rounded-lg overflow-hidden shadow-lg relative mb-2 lg:mb-3">
-            <img src="~/assets/projects/sakoku-thum.jpg" loading="lazy" alt="Photo by Austin Wade" class="w-full h-full object-center group-hover:scale-110 transition duration-200" />
+            <img src="~/assets/projects/sakoku.jpg" loading="lazy" alt="Photo by Austin Wade" class="w-full h-full object-center group-hover:scale-110 transition duration-200" />
         </nuxt-link>
 
         <div class="gap-2 px-2">
