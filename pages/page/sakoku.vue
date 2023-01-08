5dba88a63558ed44b4003ebe58bb07729e4fbcf5
ando <ando@torat.jp>
2023-01-08 18:09:38 +0900

    add

---
+++ pages/page/sakoku.vue
@@ -0,0 +1,67 @@
+<template>
+<div class="bg-white py-6 sm:py-8 lg:py-12">
+    <div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
+        <div class="mb-10 md:mb-16">
+            <h2 class="text-gray-800 text-2xl lg:text-3xl font-bold text-center mb-4 md:mb-6">SAKOKU NFTマーケットプレイス</h2>
+            <!--<p class="max-w-screen-md text-gray-500 md:text-lg text-center mx-auto">This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text but is random or otherwise generated.</p>-->
+        <div class="max-w-screen-lg px-4 md:px-8 mx-auto">
+        <div class="text-gray-800 text-lg font-semibold mb-3">Description</div>
+            <p class="text-gray-500">
+SAKOKU NFTマーケットプレイス
+            </p>
+        </div>
+        </div>
+        </div>
+
+<div class="max-w-screen-lg px-4 md:px-8 mx-auto">
+    <table class="table-auto text-left w-full">
+    <thead>
+        <tr>
+            <th class="border px-4 py-2">タイトル</th>
+            <td class="border px-4 py-2">SAKOKU NFTマーケットプレイス</td>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <th class="border px-4 py-2">クライアント</th>
+            <td class="border px-4 py-2">株式会社TORAT</td>
+        </tr>
+        <tr>
+            <th class="border px-4 py-2">Link</th>
+            <td class="border px-4 py-2">https://sakoku.world/</td>
+        </tr>
+        <tr>
+            <th class="border px-4 py-2">Scale</th>
+            <td class="border px-4 py-2">社内メンバー2名</td>
+        </tr>
+        <tr>
+            <th class="border px-4 py-2">担当</th>
+            <td class="border px-4 py-2">
+            サイト設計、サイトデザイン、サイトフロントコーディング
+            </td>
+        </tr>
+        <tr>
+          <th class="border px-4 py-2">開発ツール</th>
+          <td class="border px-4 py-2">
+            XD, VScode
+          </td>
+        </tr>
+        <tr>
+            <th class="border px-4 py-2">技術スタック</th>
+            <td class="border px-4 py-2">Laravel, Solidity, SCSS</td>
+        </tr>
+        </tbody>
+    </table>
+    </div>
+
+</template>
+
+<script>
+export default {
+    layout: 'sub'
+}
+</script>
+
+<style>
+
+</style>
